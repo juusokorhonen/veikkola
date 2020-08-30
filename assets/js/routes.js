1383a922967c09d434c147d646c60b22e82afecc
Juuso Korhonen <juuso@turqoosi.net>
2020-08-30 11:02:34 +0300

    WIP: topo images automation

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -1,16 +1,19 @@
 var hoverRoute = function(route_id) {
-	$('#' + route_id).addClass('hover');
+	//$('#' + route_id).addClass('hover');
+	$('path[data-route-id='+route_id+']').addClass('hover');
 	$('li[data-route-id='+route_id+']').addClass('hover');
 };
 var unHoverRoute = function(route_id) {
-	$('#' + route_id).removeClass('hover');
-	$('li[data-route-id='+route_id+']').removeClass('hover');	
+	//$('#' + route_id).removeClass('hover');
+	$('path[data-route-id='+route_id+']').removeClass('hover');
+	$('li[data-route-id='+route_id+']').removeClass('hover');
 };
 var setActiveRoute = function(route_id) {
 	$('.topo-img-overlay-wrap path').removeClass('active');
 	$('ul.routes li').removeClass('active');
-	$('#' + route_id).addClass('active');
-	$('li[data-route-id='+route_id+']').addClass('active');		
+	//$('#' + route_id).addClass('active');
+	$('path[data-route-id='+route_id+']').addClass('active');	
+	$('li[data-route-id='+route_id+']').addClass('active');	
 };
 $(function() {
 	// Document loaded
@@ -24,13 +27,18 @@ $(function() {
 		}
 	}
 	$('.topo-img-overlay-wrap svg path').hover(function() {
-		hoverRoute(this.id);
+		//hoverRoute(this.id);
+		//console.log(this);
+		//console.log($(this).attr('data-route-id'));
+		hoverRoute($(this).attr('data-route-id'));
 	});
 	$('.topo-img-overlay-wrap svg path').mouseout(function() {
-		unHoverRoute(this.id);
+		//unHoverRoute(this.id);
+		unHoverRoute($(this).attr('data-route-id'));
 	});
 	$('.topo-img-overlay-wrap svg path').click(function() {
-		setActiveRoute(this.id);
+		//setActiveRoute(this.id);
+		setActiveRoute($(this).attr('data-route-id'));
 	});
 	$('ul.routes li').hover(function() {
 		hoverRoute($(this).attr('data-route-id'));
